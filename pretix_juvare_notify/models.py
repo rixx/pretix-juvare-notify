f46fdfafa1676526cda638fcf7bfdf93c202c8f4
Tobias Kunze <r@rixx.de>
2021-05-10 23:52:10 +0200

    Add reminder tracking model

---
+++ pretix_juvare_notify/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _, pgettext_lazy
+from django_scopes import ScopedManager
+from pretix.base.models.event import SubEvent
+
+
+class SubEventReminder(models.Model):
+    subevent = models.ForeignKey(
+        SubEvent,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        verbose_name=pgettext_lazy("subevent", "Date"),
+    )
+    STATUS_STARTED = "s"
+    STATUS_FINISHED = "f"
+    STATUS_CHOICE = (
+        (STATUS_STARTED, _("started")),
+        (STATUS_FINISHED, _("finished")),
+    )
+    status = models.CharField(
+        max_length=3,
+        choices=STATUS_CHOICE,
+        verbose_name=_("Status"),
+        default="s",
+        db_index=True,
+    )
+    objects = ScopedManager(organizer="subevent__event__organizer")
